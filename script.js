5f48073fd41e384130f2486159c74a361f1de241
monk3yd <jegajardog@gmail.com>
2022-09-13 01:47:11 -0300

    Success, hover changing bg color

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 container = document.querySelector(".container");
 
-// Create 16 child-divs within container div
+// Create 16x16 child-divs within container div
 for (let i = 0; i < 16; i++) {
     rowDiv = document.createElement("div");
-    // Give them a class for styling with CSS
-    rowDiv.classList.add("row");
     rowDiv.style.cssText = `display: flex;
         justify-content: space-around; 
         flex: 1;
@@ -13,16 +11,21 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         cellDiv = document.createElement("div");
         cellDiv.classList.add("cell");
-        cellDiv.style.cssText = "flex: 1; text-align: center;"
-
-        cellDiv.style.border = "1px solid gray";
+        cellDiv.style.cssText = `flex: 1; text-align: center;
+            border: 1px solid gray;`;
 
         rowDiv.appendChild(cellDiv);
     }
-
-    // rowDiv.textContent = `${i}`;
-    // childDiv.style.border = "1px solid blue";
-
     container.appendChild(rowDiv);
 }
+
+const cells = document.querySelectorAll(".cell");
+cells.forEach(cell => cell.addEventListener("mouseover", () => {
+    cell.classList.add("hovered");
+}));
+
+function hoverOut() {
+    this.classList.remove("hovered");
+}
+cells.forEach(cell => cell.addEventListener("mouseout", hoverOut));
 
