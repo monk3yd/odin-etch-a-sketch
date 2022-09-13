9cb77e3ba0f25b6761732e6aaf91a1f5bdcf0f34
monk3yd <jegajardog@gmail.com>
2022-09-13 02:35:58 -0300

    Done, project working but need work on DRY

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
-container = document.querySelector(".container");
+let height = 16;
+let width = 16;
 
-// Create 16x16 child-divs within container div
-for (let i = 0; i < 16; i++) {
-    rowDiv = document.createElement("div");
-    rowDiv.style.cssText = `display: flex;
-        justify-content: space-around; 
-        flex: 1;
-        gap: 1rem;`
+let body = document.querySelector("body");
+const container = document.querySelector(".container");
 
-    for (let j = 0; j < 16; j++) {
-        cellDiv = document.createElement("div");
-        cellDiv.classList.add("cell");
-        cellDiv.style.cssText = `flex: 1; text-align: center;
-            border: 1px solid gray;`;
-
-        rowDiv.appendChild(cellDiv);
-    }
-    container.appendChild(rowDiv);
-}
+createGrid(height, width, container);
 
 const cells = document.querySelectorAll(".cell");
 cells.forEach(cell => cell.addEventListener("mouseover", () => {
@@ -29,3 +16,51 @@ cells.forEach(cell => cell.addEventListener("mouseover", () => {
 // }
 // cells.forEach(cell => cell.addEventListener("mouseout", hoverOut));
 
+button = document.createElement("button");
+button.textContent = "New Grid";
+button.style.cssText = "margin-top: 1rem;";
+button.addEventListener("click", () => {
+    // Erase grid
+    const rows = document.querySelectorAll(".row");
+    rows.forEach(row => {
+        container.removeChild(row);
+    });
+
+    // Ask for new grid dimensions
+    do {
+        height = prompt("Squares height:");
+        width = prompt("Squares width:");
+    } while (width > 100 || height > 100)
+    
+    // Redraw new dimension grid
+    createGrid(height, width, container);
+
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => cell.addEventListener("mouseover", () => {
+        cell.classList.add("hovered");
+    }));
+})
+
+body.appendChild(button);
+
+function createGrid(height, width, container) {
+    // Create 16x16 child-divs within container div
+    for (let i = 0; i < height; i++) {
+        rowDiv = document.createElement("div");
+        rowDiv.classList.add("row");
+        rowDiv.style.cssText = `display: flex;
+            justify-content: space-around; 
+            flex: 1;
+            gap: 1rem;`
+
+        for (let j = 0; j < width; j++) {
+            cellDiv = document.createElement("div");
+            cellDiv.classList.add("cell");
+            cellDiv.style.cssText = `flex: 1; text-align: center;
+                border: 1px solid gray;`;
+
+            rowDiv.appendChild(cellDiv);
+        }
+        container.appendChild(rowDiv);
+    }
+};
