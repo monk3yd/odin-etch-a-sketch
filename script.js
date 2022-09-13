d59f2be0da2cb07ca274ff95b9ac9fea3ec71d1d
monk3yd <jegajardog@gmail.com>
2022-09-13 01:02:07 -0300

    16x16 grid done, need beautifying

--- script.js
+++ script.js
@@ -2,13 +2,26 @@ container = document.querySelector(".container");
 
 // Create 16 child-divs within container div
 for (let i = 0; i < 16; i++) {
-    childDiv = document.createElement("div");
-    childDiv.classList.add("child");
+    rowDiv = document.createElement("div");
+    // Give them a class for styling with CSS
+    rowDiv.classList.add("row");
+    rowDiv.style.display = "flex";
+    rowDiv.style.justifyContent = "space-around";
 
-    childDiv.textContent = `${i}`;
-    childDiv.style.border = "1px solid blue";
+    for (let j = 0; j < 16; j++) {
+        cellDiv = document.createElement("div");
+        cellDiv.classList.add("cell");
 
-    container.appendChild(childDiv);
+        // cellDiv.textContent = `${j}`;
+        cellDiv.textContent = "cell";
+        cellDiv.style.border = "1px solid gray";
+
+        rowDiv.appendChild(cellDiv);
+    }
+
+    // rowDiv.textContent = `${i}`;
+    // childDiv.style.border = "1px solid blue";
+
+    container.appendChild(rowDiv);
 }
 
-// Give them a class for styling with CSS
